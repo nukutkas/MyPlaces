6153d46c0ff4f704c9a21dfde7c0da9a1eab4899
Nikita Kochetov <nothsmple@mail.ru>
2020-08-27 17:02:57 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Татьяна Кочетова on 27.08.2020.
+//  Copyright © 2020 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
